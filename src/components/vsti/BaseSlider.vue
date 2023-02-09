eafb07184b4b9a8a56c4161d678c011f7b88136b
Lee Junmin <terabyte4096@gmail.com>
2023-02-09 13:58:28 +0000

    add: BaseSlider

---
+++ src/components/vsti/BaseSlider.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const props = defineProps({
+    min: Number,
+    max: Number,
+    value: Number,
+});
+
+</script>
+
+<template>
+    <input type="range" :min="min" :max="max" :value="value" @input="$emit('input', value)" />
+    <div>{{ value }}</div>
+</template>
+
+<style scoped>
+
+
+</style>
