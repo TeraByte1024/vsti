538cb5d374ac9b794f4d2188deb0d96b4624a9d8
Lee Junmin <terabyte4096@gmail.com>
2023-02-22 07:07:48 +0000

    refactor: Move frequency table to store.ts

--- src/components/vsti/KeyBoard.vue
+++ src/components/vsti/KeyBoard.vue
@@ -13,45 +13,7 @@ const props = defineProps<{
     keyBinds: KeyBind[]
 }>();
 
-const frequency:{[index:string]:number} = {
-    "C3":   130.81,
-    "C#3":  138.59,
-    "D3":   146.83,
-    "D#3":  155.56,
-    "E3":   164.81,
-    "F3":   174.61,
-    "F#3":  185.00,
-    "G3":   196.00,
-    "G#3":  207.65,
-    "A3":   220.00,
-    "A#3":  233.08,
-    "B3":   246.94,
-    "C4":   261.63,
-    "C#4":  277.18,
-    "D4":   293.66,
-    "D#4":  311.13,
-    "E4":   329.63,
-    "F4":   349.23,
-    "F#4":  369.99,
-    "G4":   392.00,
-    "G#4":  415.30,
-    "A4":   440.00,
-    "A#4":  466.16,
-    "B4":   493.88,
-    "C5":   523.25,
-    "C#5":  554.37,
-    "D5":   587.33,
-    "D#5":  622.25,
-    "E5":   659.25,
-    "F5":   698.46,
-    "F#5":  739.99,
-    "G5":   783.99,
-    "G#5":  830.61,
-    "A5":   880.00,
-    "A#5":  932.33,
-    "B5":   987.77,
-    "C6":  1046.50,
-}
+const frequency = store.state.frequencyTable;
 
 const isPlaying:{[index:string]: boolean} = reactive({});
 
@@ -73,16 +35,6 @@ function pressedKey(key: string) {
 function releasedKey(key: string) {
     isPlaying[key] = false;
 }
-
-const wavetable:{label:string, value:any}[] = [
-    { label: "Sine", value: "sine" },
-    { label: "Square", value: "square" },
-    { label: "Saw", value: "sawtooth" },
-    { label: "Triangle", value: "triangle" },
-    { label: "Custom", value: "custom" },
-]
-
-const waveform:Ref<OscillatorType> = ref<OscillatorType>('sine');
 
 const audioContext = store.state.audioContext;
 const gainNode = audioContext.createGain();
@@ -162,6 +114,16 @@ const envelope: EnvelopeProps = {
     sustain: {velocity: ref<number>(0.4),},
     release: {duration: ref<number>(0.5)}
 }
+
+const wavetable:{label:string, value:any}[] = [
+    { label: "Sine", value: "sine" },
+    { label: "Square", value: "square" },
+    { label: "Saw", value: "sawtooth" },
+    { label: "Triangle", value: "triangle" },
+    { label: "Custom", value: "custom" },
+]
+
+const waveform:Ref<OscillatorType> = ref<OscillatorType>('sine');
 
 function connectGain(audioNode: AudioNode) {
     audioNode.connect(gainNode);
