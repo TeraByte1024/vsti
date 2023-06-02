961e6a850bcf646617b6fa41163c74fc274414be
Lee junmin <terabyte4096@gmail.com>
2023-06-03 04:50:01 +0900

    chore: Update global style config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{vue,html,js,ts}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        accent: "#354649",
+        primary: "#758295",
+        secondary: "#E5EDEE",
+        focus: "#D4DCDD",
+        success: "#A3C6C4",
+      },
+    },
   },
   plugins: [],
 };
